74dad9484facc0e5c8aa34755464c906b496bda6
Drew Hans <drewhans555@gmail.com>
2016-12-17 00:29:20 -0600

    Update and rename driver.py to CodeDriver.py

--- CodeDriver.py
+++ CodeDriver.py
@@ -3,15 +3,17 @@ OpenCV Documentation:  http://docs.opencv.org/3.0-beta/doc/py_tutorials/
 Numpy and Scipy Documentation:  https://docs.scipy.org/doc/
 '''
 
+import numpy as np
 import cv2
 import imutils
 
 
 class CodeDriver:
-    WEBCAM_WIDTH = 1280
-    WEBCAM_HEIGHT = 720
+    # global variables
+    WEBCAM_WIDTH = 1280  # used to manually set the webcam capture width
+    WEBCAM_HEIGHT = 720  # used to manually set the webcam capture height
     EPSILON_SCALAR = 0.04  # precision scalar for approximating polygon contours
-    RESIZE_WIDTH = 800  # width in pixels for resizing images to a more managable size
+    RESIZE_WIDTH = 600  # width in pixels for resizing images to a more managable size
 
     # defines constructor for CodeDriver class
     def __init__(self):
@@ -25,59 +27,52 @@ class CodeDriver:
     def main(self):
         print("CodeDriver.main was called.")
 
-        print("Calling start_webcam method...")
+        print("Calling CodeDriver.start_webcam method...")
         self.start_webcam()
 
         # use opencv to read an image
-        # originalimg = cv2.imread('testimages/testimg02.bmp')
-        originalimg = cv2.imread('test.bmp')
+        # original_image = cv2.imread('testimages/testimg02.bmp')
+        original_image = cv2.imread('test.bmp')
         print("image was read from project directory.")
 
         # show originalimg for testing purposes
-        cv2.imshow("Original Image Captured From Webcam", originalimg)
+        cv2.imshow("Original Image Captured From Webcam", original_image)
         cv2.waitKey(0)  # wait for key press when <= 0
 
         # use imutils to resize originalimg to a smaller factor so that the shapes can be approximated better
-        resizedimg = imutils.resize(originalimg, width=self.RESIZE_WIDTH)
+        resized_image = imutils.resize(original_image, width=self.RESIZE_WIDTH)
 
         # save the ratio of originalimg / resizedimg for drawing contours later
-        ratio = originalimg.shape[0] / float(resizedimg.shape[0])
+        ratio = original_image.shape[0] / float(resized_image.shape[0])
 
-        print("Calling binarization method...")
-        binaryimg = self.binarization(resizedimg)
+        print("Calling CodeDriver.binarization method...")
+        binary_image = self.binarization(resized_image)
 
-        # show threshimg for testing purposes
-        cv2.imshow("Image After Resize, Grayscale, GaussianBlur, OtsuThresholding, and Inversion", binaryimg)
+        # show binaryimg for testing purposes
+        cv2.imshow("Image After Resize, Grayscale, GaussianBlur, OtsuThreshold, and Inversion", binary_image)
         cv2.waitKey(0)  # wait for key press when <= 0
 
-        print("Calling find_contours method...")
-        contours = self.find_contours(binaryimg)
+        print("Calling CodeDriver.find_contours method...")
+        list_of_contours = self.find_contours(binary_image)
 
-        print("Calling identify_shape method...")
-        self.identify_shape(originalimg, ratio, contours)
+        print("Calling CodeDriver.identify_shape method...")
+        self.identify_shape(original_image, ratio, list_of_contours)
 
     def start_webcam(self):
         print("CodeDriver.start_webcam was called.")
         loop = True  # used for looping over webcam frames
 
-        # link to the webcam and store that link to webcam variable
-        webcam = cv2.VideoCapture(0)
-
+        webcam = cv2.VideoCapture(0)  # link to the webcam and store that link to webcam variable
         webcam.set(3, self.WEBCAM_WIDTH)  # set video feed width
         webcam.set(4, self.WEBCAM_HEIGHT)  # set video feed height
 
         print("Begin VideoCapture and show webcam frames in window.")
 
         while loop:
-            # capture image from the webcam frame-by-frame
-            ret, frame = webcam.read()
+            ret, frame = webcam.read()  # capture image from the webcam frame-by-frame
+            cv2.imshow('frame', frame)  # display the resulting frame from the webcam
 
-            # operations on the frame from the webcam start here
-
-            # display the resulting frame from the webcam
-            cv2.imshow('frame', frame)
-
-            # button listeners
+            # key-press listeners
             # c key listener - if c key is pressed then save the frame as a bmp image
             if cv2.waitKey(1) & 0xFF == ord('c'):
                 print("Detected c key press.")
@@ -94,70 +89,69 @@ class CodeDriver:
                 print("Detected window exit button press.")
                 loop = False
 
-        # (5) release the capture and then destroy all windows after leaving loop
-        webcam.release()
-        cv2.destroyAllWindows()
+        webcam.release()  # release the capture after leaving loop
+        cv2.destroyAllWindows()  # destroy all open windows before proceeding
         print("Released webcam and used destroyAllWindows.")
 
-    def binarization(self, image):
+    def binarization(self, resized_image):
         print("CodeDriver.binarization was called.")
 
-        # convert resizedimg to grayscale
-        grayimg = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # convert resized_image to grayscale
+        gray_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
 
-        # blur grayimg to decrease pixel "noise" (for low quality images)
-        gaussianblurred = cv2.GaussianBlur(grayimg, (5, 5), 0)
+        # blur gray_image with a Gaussian Function to decrease pixel "noise"
+        blur_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
 
-        # perform otsu binarization - threshhold gaussianblurred with THRESH_BINARY
-        otsuthreshimg = cv2.threshold(gaussianblurred, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+        # binarize blur_image with Otsu's Threshold Method
+        otsuthreshold_image = cv2.threshold(blur_image, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
-        # threshhold bilateralfilterblurred with THRESH_BINARY_INV
-        threshimg = cv2.threshold(otsuthreshimg, 128, 255, cv2.THRESH_BINARY_INV)[1]
+        # invert otsuthreshold_image to make background black and polygons white (for findContours to work)
+        invert_image = cv2.threshold(otsuthreshold_image, 128, 255, cv2.THRESH_BINARY_INV)[1]
 
-        return threshimg
+        return invert_image
 
-    def find_contours(self, threshimg):
+    def find_contours(self, binary_image):
         print("CodeDriver.find_contours was called.")
-
-        # find contours in threshimg
+        
         # parameters - findContours(InputOutputArray image, int retrievalMode, int ContourApproximationMethod)
-        foundcontours = cv2.findContours(threshimg.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        countours = foundcontours[1]  # note if using opencv1 use foundcontours[0]
+        found_contours = cv2.findContours(binary_image.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        countours = found_contours[1]  # note if using opencv1 use foundcontours[0]
         return countours
 
-    def identify_shape(self, originalimg, ratio, threshimgcontours):
+    def identify_shape(self, original_image, ratio, list_of_contours):
         print("CodeDriver.identify_shape was called.")
 
-        # for every group of contours (shape found) drawContours and putText over originalimg
-        for c in threshimgcontours:
+        # for every group of contours (potential polygon) in list_of_contours
+        for c in list_of_contours:
             moments = cv2.moments(c)  # a weighted average (moment) of the image pixels' intensities
-            centerx = int((moments['m10'] / (moments['m00'] + 0.00001)) * ratio)  # find x-axis center of shape
-            centery = int((moments['m01'] / (moments['m00'] + 0.00001)) * ratio)  # find y-axis center of shape
+            centerx = int((moments['m10'] / (moments['m00'] + 0.00001)) * ratio)  # find x-axis center of polygon
+            centery = int((moments['m01'] / (moments['m00'] + 0.00001)) * ratio)  # find y-axis center of polygon
+            
+            print("Calling CodeDriver.detect_shape method...")
             shapename = self.detect_shape(c)
 
-            # adjust the contour bounds found on the resized image to the original image
+            # adjust the contour bounds found on the resized_image to the original_image
             c = c.astype("float")  # numpy casting to float for more precise adjustment
-            c = c * ratio  # adjust the contour
+            c = c * ratio  # apply adjustment to the contour
             c = c.astype("int")  # numpy casting to int for cv2.drawContours
 
-            # parameters - drawContours(image, InputArrayOfArrays contours, color (R,G,B), thickness)
-            cv2.drawContours(originalimg, [c], -1, (21, 101, 192), 2)
+            # parameters - drawContours(image, InputArrayOfArrays contours, contourIdx, color (R,G,B), thickness)
+            cv2.drawContours(original_image, [c], -1, (21, 101, 192), 2)
 
             # parameters - putText(image, string, Point (xpos, ypos), fontFace, fontScale, color (R,G,B), thickness)
-            cv2.putText(originalimg, shapename, (centerx, centery), cv2.FONT_HERSHEY_PLAIN, 1, (192, 112, 21), 2)
+            cv2.putText(original_image, shapename, (centerx, centery), cv2.FONT_HERSHEY_PLAIN, 1, (192, 112, 21), 2)
 
-            cv2.imshow("Original Image With ShapeName & Contour Overlay", originalimg)
-        cv2.waitKey(0)
+            cv2.imshow("Original Image With ShapeName & Contour Overlay", original_image)
+        cv2.waitKey(0)  # wait for key press when <= 0
 
-    def detect_shape(self, contour):
+    def detect_shape(self, contours):
         print("CodeDriver.detect_shape was called.")
-
         shapename = "unknown"  # initialize shapename
 
         # approximate the contour along the perimeter of the shape
-        perimeter = cv2.arcLength(contour, True)  # returns perimeter of the shape
+        perimeter = cv2.arcLength(contours, True)  # returns perimeter of the shape
         epsilon = self.EPSILON_SCALAR * perimeter  # accuracy parameter - the maximum distance from contour to approximation
-        approximation = cv2.approxPolyDP(contour, epsilon, True)  # approximates the real contours with epsilon (note true for close shapes, false for open shapes)
+        approximation = cv2.approxPolyDP(contours, epsilon, True)  # approximates the real contours with epsilon (note true for close shapes, false for open shapes)
 
         if len(approximation) == 3:
             # if the approximation finds 3 vertices then a triangle has been found
